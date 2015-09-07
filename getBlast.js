41b38e7f3a52405ecf071f574b065c638220057d
Martin Page <wookoouk@gmail.com>
2015-09-07 17:09:39 +0100

    getBlast update

--- getBlast.js
+++ getBlast.js
@@ -8,13 +8,41 @@ var address = '/blast/executables/blast+/LATEST/';
 var platform = OS.platform();
 var arch = OS.arch();
 
-console.log(platform, arch);
+if (platform === 'darwin') {
+  platform = 'macosx';
+  arch = 'universal';
+}
+
+var foundIt = false;
+var fileName = '';
+
+console.log('looking for', platform, arch, '...');
 
 var c = new Client();
 c.on('ready', function () {
-  c.list(function (err, list) {
+  c.list(address, function (err, list) {
     if (err) throw err;
-    console.dir(list);
+    //console.dir(list);
+
+
+    list.forEach(function (l) {
+      if (l.name.indexOf(platform) > -1) {
+        if (l.name.indexOf(arch) > -1) {
+          if (l.name.indexOf('md5') === -1) {
+            //console.log(l.name, 'this is the one!');
+            foundIt = true;
+            fileName = l.name;
+          }
+        }
+      }
+    });
+    if (!foundIt) {
+      console.error('we could not find the correct version of blast+ for you, sorry');
+    } else {
+
+      var finalURL = tt + address + fileName;
+      console.log('DOWNLOAD', finalURL);
+    }
     c.end();
   });
 });
